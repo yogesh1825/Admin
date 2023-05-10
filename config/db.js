edb5120079f7261f6b171c6c3ef240c40e665f31
unknown <123@IELTS.IELTS>
2023-05-10 09:54:53 +0530

    db updated

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 let connect=async()=>{
-    await mongoose.connect("mongodb://127.0.0.1:27017")
+    await mongoose.connect("mongodb+srv://rw5kuldeephs:admin@cluster0.idnftpe.mongodb.net/?retryWrites=true&w=majority")
     console.log("connected to MongoDB")
 }
 
